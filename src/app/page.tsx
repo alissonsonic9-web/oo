476cc1a5a4da21c5f63d1d094d2f1d72ecab517c
Alisson <alissondedeussiqueira@gmail.com>
2025-09-20 21:13:53 +0000

    coloque a sessão 🎁 +6 Bônus Exclusivos de baixo da de escolha seu plano

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
         <ProblemSolution />
         <Testimonials />
         <Pricing />
-        <Community />
         <Bonuses />
+        <Community />
         <Curriculum />
         <Audience />
         <Marquee />
