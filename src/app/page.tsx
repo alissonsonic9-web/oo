ebd9ed5f0b547cfe362031ba70ca7eeb94ec9795
Alisson <alissondedeussiqueira@gmail.com>
2025-09-20 21:15:15 +0000

    coloque a sessão O Que Você Vai Aprender no Curso a baixo da de 🎁 +6 Bô

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,8 +22,8 @@ export default function Home() {
         <Testimonials />
         <Pricing />
         <Bonuses />
-        <Community />
         <Curriculum />
+        <Community />
         <Audience />
         <Marquee />
         <Guarantee />
