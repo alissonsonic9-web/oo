20c3aa94e7e330e2f73182a5ad6b491556267769
Alisson <alissondedeussiqueira@gmail.com>
2025-09-23 20:46:46 +0000

    Ainda nn estar nem na versão desktop nem na versão de mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,14 +14,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className="overflow-x-hidden">
+    <html lang="pt-BR">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased overflow-x-hidden">
+      <body className="font-body antialiased">
         {children}
         <Toaster />
         <SocialProof />
