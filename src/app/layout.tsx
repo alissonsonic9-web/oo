00d2b792770c971b30c27c9ba224e9ecdcd4a5b4
Alisson <alissondedeussiqueira@gmail.com>
2025-09-24 03:43:02 +0000

    AINDA ESTAR ESSA MESMA COISA,  CORRIJA EU QUERO SO O SITE SEM PODER IR P

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased pt-16">
         <Countdown />
-        <div className="relative w-full overflow-x-hidden">
+        <div className="relative w-full">
           {children}
         </div>
         <Toaster />
