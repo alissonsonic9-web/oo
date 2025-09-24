25a731d1b9d113a7de1577cd9310db7a739e1b24
Alisson <alissondedeussiqueira@gmail.com>
2025-09-24 03:29:38 +0000

    deixe o site todo estatico sem essa opção de poder ir por lado esquerdo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,9 +22,9 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased pt-16">
+      <body className="font-body antialiased pt-16 overflow-x-hidden">
         <Countdown />
-        <div className="relative w-full overflow-x-hidden">
+        <div className="relative w-full">
           {children}
         </div>
         <Toaster />
