7c97becb4b374d26658a083f3263ddfeb7065aab
Alisson <alissondedeussiqueira@gmail.com>
2025-10-01 22:39:15 +0000

    quero o fundo na cor branca (_for element <Curriculum>_)

--- src/app/sections/Curriculum.tsx
+++ src/app/sections/Curriculum.tsx
@@ -63,7 +63,7 @@ const modules = [
 
 export default function Curriculum() {
   return (
-    <section id="curriculum" className="w-full py-12 md:py-24 lg:py-32 bg-secondary">
+    <section id="curriculum" className="w-full py-12 md:py-24 lg:py-32 bg-card">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
